a3f014d9e44e159ec6380c370cf94094589660c7
Laurent Labine <laurentlabine@gmail.com>
2021-05-14 22:43:59 +0200

    Added linked in and freecodecamp icons

---
+++ src/gatsby-theme-mate/icons.js
@@ -0,0 +1,23 @@
+import {
+  faMedium,
+  faTwitter,
+  faGithub,
+  faLinkedin,
+  faFreeCodeCamp,
+} from '@fortawesome/free-brands-svg-icons';
+import {
+  faGlobe,
+  faEnvelope,
+  faCoffee,
+} from '@fortawesome/free-solid-svg-icons';
+
+export const ICONS = [
+  faMedium,
+  faTwitter,
+  faGithub,
+  faGlobe,
+  faEnvelope,
+  faCoffee,
+  faLinkedin,
+  faFreeCodeCamp
+];
